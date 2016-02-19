5b20086de42f788e8ea62d447145a22ed348eb80
Mauro Junior <mauropereirajr@gmail.com>
2016-02-18 23:15:09 -0200

    added new functions

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict';
-const escapeRegex = require('escape-regex');
+const toFactory     = require('tofactory'),
+      escapeRegex   = require('escape-regex'),
+      bracketsRegex = /^\{.*\}$/,
+      brackets      = /[\{\}]/g,
+      paramError    = new Error('"route" parameter must be a string.'),
+      toString      = Object.prototype.toString;
 
-module.exports = easyRegex;
+module.exports = toFactory(EasyRegex);
 
 /**
  * Convert `str` to a `RegExp`.
@@ -10,24 +15,91 @@ module.exports = easyRegex;
  * @return {RegExp}
  * @api private
  */
-
-function easyRegex(str) {
-    const listStr   = [],
-          optsList  = [];
+function EasyRegex(spec) {
+    let regex, str, delimeter;
     
-    if (str instanceof RegExp) return str;
+    const listKeys  = [];
     
-    str.split(':').forEach(function (s, sIdx) {
-        listStr[sIdx] = [];
-        s.split('|').forEach(function (o, oIdx) {
-            listStr[sIdx][oIdx] = escapeRegex(o).replace(/\\\*/g, '(.+)');
-        });
-    });
-    listStr.forEach(function (list, i) {
-        let opts = list.join('|');
+    init();
+    
+    return {
+        source: regex.source,
+        test,
+        exec,
+        getArrayResult,
+        getObjectResult
+    };
+    
+    function init() {
+        if (toString.call(spec) === '[object Object]') {
+            str = spec.route;
+            delimeter = spec.delimeter;
+        } else {
+            str = spec;
+        }
         
-        if (list.length > 1) opts = '(' + opts + ')';
-        optsList[i] = opts; 
-    });
-    return new RegExp(`^${optsList.join(':')}$`);
+        if (typeof str !== 'string' || str instanceof RegExp) throw paramError;
+        
+        delimeter = delimeter || ':';
+        
+        regex = createRegex();
+    }
+    
+    function test(routeString) {
+        return regex.test(routeString);
+    }
+    
+    function exec(routeString) {
+        return regex.exec(routeString);
+    }
+    
+    function getArrayResult(routeString) {
+        return regex.exec(routeString).slice(1);
+    }
+    
+    function getObjectResult(routeString) {
+        const resultObj = {},
+              resultList = getArrayResult(routeString);
+        
+        listKeys.forEach(function (key, i) {
+            resultObj[key] = resultList[i];
+        });
+        
+        return resultObj;
+    }
+    
+    function createRegex() {
+        const optsList  = [],
+              listStr   = [];
+        
+        str.split(delimeter).forEach(function (s, sidx) {
+            let opts;
+            
+            s = s.trim();
+            listStr[sidx] = [];
+            
+            if (bracketsRegex.test(s)) {
+                let route   = s.split('=');
+                
+                listKeys[sidx] = route[0].trim().replace(brackets, '');
+                opts = route[1] ? route[1].trim().replace(brackets, '') : '*';    
+            } else {
+                opts = s;
+                listKeys[sidx] = sidx + '';
+            }
+
+            opts.split('|').forEach(function (o, i) {
+                listStr[sidx][i] = escapeRegex(o).replace(/\\\*/g, '(.+)');
+            });
+        });
+        
+        listStr.forEach(function (list, i) {
+            let opts = list.join('|');
+            
+            if (list.length > 1) opts = '(' + opts + ')';
+            optsList[i] = opts; 
+        });
+        
+        return new RegExp(`^${optsList.join(delimeter)}$`);
+    }
 }
